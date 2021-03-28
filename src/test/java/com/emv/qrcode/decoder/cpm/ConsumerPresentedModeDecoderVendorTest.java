3b0ea0bccbae413fc1762624d6dd8b04229010a2
levent.tekin <levent.tekin@payolog.com>
2021-03-28 19:29:34 +0100

    Multi application generator and decoder tests added

--- src/test/java/com/emv/qrcode/decoder/cpm/ConsumerPresentedModeDecoderVendorTest.java
+++ src/test/java/com/emv/qrcode/decoder/cpm/ConsumerPresentedModeDecoderVendorTest.java
@@ -21,7 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ConsumerPresentedModeDecoderVendorTest {
 
   @Test
-  public void testSuccessVendorDecode() throws Exception {
+  public void testVendorSuccessVendorDecode() throws Exception {
     final String encoded = "hQVDUFYwMWEYTwegAAAAd3d3UA1Nb2JpbGUgV2FsbGV0YlpfLQZhcmVuZGWfGQQSNFZ4ZEjfeQaXFYEjRWffeAIlAt93EUFiZHVsa2FyaW0gTW91bWlu33YNV2FsbGV0IElzc3Vlct91CBI0VniavN7w33QBAd9zBDEyMzQ";
 
     final ConsumerPresentedMode consumerPresentedMode = DecoderCpm.decode(encoded, ConsumerPresentedMode.class);
@@ -46,6 +46,66 @@ public class ConsumerPresentedModeDecoderVendorTest {
 
     final CommonDataTransparentTemplate commonDataTransparentTemplate = commonDataTemplate.getCommonDataTransparentTemplate();
     assertThat(commonDataTransparentTemplate.getTag(), equalTo(ConsumerPresentedModeFieldCodes.ID_COMMON_DATA_TRANSPARENT_TEMPLATE));
+    assertThat(commonDataTransparentTemplate.getVendorMobileNumber().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_MOBILE_NUMBER));
+    assertThat(commonDataTransparentTemplate.getVendorMobileNumber().getStringValue(), equalTo("971581234567"));
+    assertThat(commonDataTransparentTemplate.getVendorExpiryDate().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_EXPIRY_DATE));
+    assertThat(commonDataTransparentTemplate.getVendorExpiryDate().getStringValue(), equalTo("2502"));
+    assertThat(commonDataTransparentTemplate.getVendorName().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_NAME));
+    assertThat(commonDataTransparentTemplate.getVendorName().getStringValue(), equalTo("Abdulkarim Moumin"));
+    assertThat(commonDataTransparentTemplate.getVendorWalletIssuer().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_WALLET_ISSUER));
+    assertThat(commonDataTransparentTemplate.getVendorWalletIssuer().getStringValue(), equalTo("Wallet Issuer"));
+    assertThat(commonDataTransparentTemplate.getVendorCryptogram().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_CRYPTOGRAM));
+    assertThat(commonDataTransparentTemplate.getVendorCryptogram().getStringValue(), equalTo("123456789ABCDEF0"));
+    assertThat(commonDataTransparentTemplate.getVendorOfflineOnlineResult().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_OFFLINE_ONLINE_RESULT));
+    assertThat(commonDataTransparentTemplate.getVendorOfflineOnlineResult().getStringValue(), equalTo("01"));
+    assertThat(commonDataTransparentTemplate.getVendorPin().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_PIN));
+    assertThat(commonDataTransparentTemplate.getVendorPin().getStringValue(), equalTo("1234"));
+
+  }
+
+
+  @Test
+  public void testVendorMultiAppSuccessVendorDecode() throws Exception {
+    final String encoded = "hQVDUFYwMWEYTwegAAAAd3d3UA1Nb2JpbGUgV2FsbGV0YRZPB6AAAABVVVVQC0NhcmQgUVIgQVBQYnFaCBI0Vni" +
+            "QEjRYnxkEEjRWeGRenxAHBgEKAwAAAJ82AgABnzcEbVjvE995BpcVgSNFZ994AiUC33cRQWJkdWxrYXJpbSBNb3" +
+            "VtaW7fdg1XYWxsZXQgSXNzdWVy33UIEjRWeJq83vDfdAEB33MEMTIzNA==";
+
+    final ConsumerPresentedMode consumerPresentedMode = DecoderCpm.decode(encoded, ConsumerPresentedMode.class);
+
+    final PayloadFormatIndicator payloadFormatIndicator = consumerPresentedMode.getPayloadFormatIndicator();
+    assertThat(payloadFormatIndicator.getTag(), equalTo(ConsumerPresentedModeFieldCodes.ID_PAYLOAD_FORMAT_INDICATOR));
+    assertThat(payloadFormatIndicator.getStringValue(), equalTo("CPV01"));
+
+    final ApplicationTemplate applicationTemplate1 = consumerPresentedMode.getApplicationTemplates().get(0);
+    assertThat(applicationTemplate1.getTag(), equalTo(ConsumerPresentedModeFieldCodes.ID_APPLICATION_TEMPLATE));
+    assertThat(applicationTemplate1.getApplicationDefinitionFileName().getTag(), equalTo(TagTransactionProcessingCodes.ID_APPLICATION_DEFINITION_FILE_NAME));
+    assertThat(applicationTemplate1.getApplicationDefinitionFileName().getStringValue(), equalTo("A0000000777777"));
+    assertThat(applicationTemplate1.getApplicationLabel().getTag(), equalTo(TagTransactionProcessingCodes.ID_APPLICATION_LABEL));
+    assertThat(applicationTemplate1.getApplicationLabel().getStringValue(), equalTo("Mobile Wallet"));
+
+    final ApplicationTemplate applicationTemplate2 = consumerPresentedMode.getApplicationTemplates().get(1);
+    assertThat(applicationTemplate2.getTag(), equalTo(ConsumerPresentedModeFieldCodes.ID_APPLICATION_TEMPLATE));
+    assertThat(applicationTemplate2.getApplicationDefinitionFileName().getTag(), equalTo(TagTransactionProcessingCodes.ID_APPLICATION_DEFINITION_FILE_NAME));
+    assertThat(applicationTemplate2.getApplicationDefinitionFileName().getStringValue(), equalTo("A0000000555555"));
+    assertThat(applicationTemplate2.getApplicationLabel().getTag(), equalTo(TagTransactionProcessingCodes.ID_APPLICATION_LABEL));
+    assertThat(applicationTemplate2.getApplicationLabel().getStringValue(), equalTo("Card QR APP"));
+
+    final CommonDataTemplate commonDataTemplate = consumerPresentedMode.getCommonDataTemplate();
+    assertThat(commonDataTemplate.getTag(), equalTo(ConsumerPresentedModeFieldCodes.ID_COMMON_DATA_TEMPLATE));
+    assertThat(commonDataTemplate.getApplicationPAN().getTag(), equalTo(TagTransactionProcessingCodes.ID_APPLICATION_PAN));
+    assertThat(commonDataTemplate.getApplicationPAN().getStringValue(), equalTo("1234567890123458"));
+    assertThat(commonDataTemplate.getTokenRequestorID().getTag(), equalTo(TagTransactionProcessingCodes.ID_TOKEN_REQUESTOR_ID));
+    assertThat(commonDataTemplate.getTokenRequestorID().getStringValue(), equalTo("12345678"));
+
+    final CommonDataTransparentTemplate commonDataTransparentTemplate = commonDataTemplate.getCommonDataTransparentTemplate();
+    assertThat(commonDataTransparentTemplate.getTag(), equalTo(ConsumerPresentedModeFieldCodes.ID_COMMON_DATA_TRANSPARENT_TEMPLATE));
+    assertThat(commonDataTransparentTemplate.getIssuerApplicationData().getTag(), equalTo(TagTransactionProcessingCodes.ID_ISSUER_APPLICATION_DATA));
+    assertThat(commonDataTransparentTemplate.getIssuerApplicationData().getStringValue(), equalTo("06010A03000000"));
+    assertThat(commonDataTransparentTemplate.getApplicationTransactionCounter().getTag(), equalTo(TagTransactionProcessingCodes.ID_APPLICATION_TRANSACTION_COUNTER));
+    assertThat(commonDataTransparentTemplate.getApplicationTransactionCounter().getStringValue(), equalTo("0001"));
+    assertThat(commonDataTransparentTemplate.getUnpredictableNumber().getTag(), equalTo(TagTransactionProcessingCodes.ID_UNPREDICTABLE_NUMBER));
+    assertThat(commonDataTransparentTemplate.getUnpredictableNumber().getStringValue(), equalTo("6D58EF13"));
+
     assertThat(commonDataTransparentTemplate.getVendorMobileNumber().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_MOBILE_NUMBER));
     assertThat(commonDataTransparentTemplate.getVendorMobileNumber().getStringValue(), equalTo("971581234567"));
     assertThat(commonDataTransparentTemplate.getVendorExpiryDate().getTag(), equalTo(TagTransactionProcessingCodes.ID_VENDOR_EXPIRY_DATE));
--- src/test/java/com/emv/qrcode/model/cpm/ConsumerPresentedModeVendorTest.java
+++ src/test/java/com/emv/qrcode/model/cpm/ConsumerPresentedModeVendorTest.java
@@ -14,7 +14,7 @@ public class ConsumerPresentedModeVendorTest {
 
 
   @Test
-  public void testSuccessToHexWithApplicationTemplate() throws IOException {
+  public void testVendorSuccessToHexWithApplicationTemplate() throws IOException {
     final ConsumerPresentedMode consumerPresentedMode = new ConsumerPresentedMode();
 
     // 61 Application Level
@@ -52,6 +52,63 @@ public class ConsumerPresentedModeVendorTest {
 
     assertThat(consumerPresentedMode.toBase64(), equalTo(
           "hQVDUFYwMWEYTwegAAAAd3d3UA1Nb2JpbGUgV2FsbGV0YlpfLQZhcmVuZGWfGQQSNFZ4ZEjfeQaXFYEjRWffeAIlAt93EUFiZHVsa2FyaW0gTW91bWlu33YNV2FsbGV0IElzc3Vlct91CBI0VniavN7w33QBAd9zBDEyMzQ="));
+  }
+
+
+  @Test
+  public void testVendorMultiAppSuccessToHexWithApplicationTemplate() throws IOException {
+    final ConsumerPresentedMode consumerPresentedMode = new ConsumerPresentedMode();
+
+    // 61 Application Level
+    final ApplicationTemplate applicationTemplate1 = new ApplicationTemplate();
+    applicationTemplate1.setApplicationDefinitionFileName("A0000000777777");
+    applicationTemplate1.setApplicationLabel("Mobile Wallet");
+
+    final ApplicationTemplate applicationTemplate2 = new ApplicationTemplate();
+    applicationTemplate2.setApplicationDefinitionFileName("A0000000555555");
+    applicationTemplate2.setApplicationLabel("Card QR APP");
+
+    // 64 Specific data
+    final CommonDataTransparentTemplate commonDataTransparentTemplate = new CommonDataTransparentTemplate();
+    commonDataTransparentTemplate.setIssuerApplicationData("06010A03000000");
+    //commonDataTransparentTemplate.setApplicationCryptogram("584FD385FA234BCC");
+    commonDataTransparentTemplate.setApplicationTransactionCounter("0001");
+    commonDataTransparentTemplate.setUnpredictableNumber("6D58EF13");
+
+    commonDataTransparentTemplate.setVendorMobileNumber("971581234567");
+    commonDataTransparentTemplate.setVendorExpiryDate("2502");
+    commonDataTransparentTemplate.setVendorName("Abdulkarim Moumin");
+    commonDataTransparentTemplate.setVendorWalletIssuer("Wallet Issuer");
+    commonDataTransparentTemplate.setVendorCryptogram("123456789ABCDEF0");
+    commonDataTransparentTemplate.setVendorOfflineOnlineResult("01");
+    commonDataTransparentTemplate.setVendorPin("1234");
+
+    // 62 Common Data
+    final CommonDataTemplate commonDataTemplate = new CommonDataTemplate();
+    commonDataTemplate.setApplicationPAN("1234567890123458");
+    //commonDataTemplate.setCardholderName("CARDHOLDER/EMV");
+    //commonDataTemplate.setLanguagePreference("arende");
+    commonDataTemplate.setTokenRequestorID("12345678");
+    commonDataTemplate.setCommonDataTransparentTemplate(commonDataTransparentTemplate);
+
+    consumerPresentedMode.setPayloadFormatIndicator(new PayloadFormatIndicator());
+    consumerPresentedMode.addApplicationTemplate(applicationTemplate1);
+    consumerPresentedMode.addApplicationTemplate(applicationTemplate2);
+    consumerPresentedMode.setCommonDataTemplate(commonDataTemplate);
+
+    assertThat(consumerPresentedMode.toHex(),
+            equalTo("85054350563031" +
+                    "61184F07A0000000777777500D4D6F62696C652057616C6C6574" +
+                    "61164F07A0000000555555500B4361726420515220415050" +
+                    "62715A0812345678901234589F190412345678" +
+                    "645E" +
+                    "9F100706010A030000009F360200019F37046D58EF13" +
+                    "DF7906971581234567DF78022502DF7711416264756C6B6172696D204D6F756D696EDF760D57616C6C657420497373756572DF7508123456789ABCDEF0DF740101DF730431323334"));
+
+    assertThat(consumerPresentedMode.toBase64(), equalTo(
+            "hQVDUFYwMWEYTwegAAAAd3d3UA1Nb2JpbGUgV2FsbGV0YRZPB6AAAABVVVVQC0NhcmQgUVIgQVBQYnFaCBI0Vni" +
+                    "QEjRYnxkEEjRWeGRenxAHBgEKAwAAAJ82AgABnzcEbVjvE995BpcVgSNFZ994AiUC33cRQWJkdWxrYXJpbSBNb3" +
+                    "VtaW7fdg1XYWxsZXQgSXNzdWVy33UIEjRWeJq83vDfdAEB33MEMTIzNA=="));
   }
 
 
